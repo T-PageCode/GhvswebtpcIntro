dec7f0da8299410c0b755ec19f5e7c0a95e028ff
T-Color <cdt20260@outlook.com>
2026-07-03 18:18:53 +0800

    first commit

---
+++ Script/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("contextmenu",(NoCtm) => {
+    NoCtm.preventDefault();
+})
+let BlackDiv1 = document.getElementById("BlackDiv1");
+function BlackDiv1Show() {
+    BlackDiv1.style.visibility = "visible";
+    BlackDiv1.style.opacity = "1";
+    setTimeout(() => {
+        location.href="https://t-pagecode.github.io/Ghvswebtpc/WebSystem/InstAndGo.html"
+    },1000)
+}
